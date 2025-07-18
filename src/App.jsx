4eb9cd330bb22c13b8331cd77da499fc4a6d5366
zoyakhtar8400 <zoyaakhtar325@gmail.com>
2025-07-18 17:19:00 +0530

    dashboad page of buddha avenue and booking calender component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import AppContextProvider from "./context/AppContext";
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
+import Dashboard from "./components/Dashboard";
+import BookingCalendar from "./components/BookingCalendar";
 
 const App = () => {
   return (
@@ -10,6 +12,12 @@ const App = () => {
         <Sidebar />
         <div className="flex-1 flex flex-col overflow-hidden">
           <Header />
+          <main className="flex-1 overflow-hidden">
+            <Dashboard />
+            <div className="min-h-screen bg-gray-50 p-6">
+              <BookingCalendar />
+            </div>
+          </main>
         </div>
       </div>
     </AppContextProvider>
